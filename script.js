3b2a07ab874d213f6835c77216c1b80566432bba
aarikpokras <85502246+aarikpokras@users.noreply.github.com>
2022-04-25 09:31:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function start() {
 	alert("This is a thank you bot. It was based on GitHub/Anti-Apple4life's tymaker.");
 	var one = prompt("Who are you writing to?");
   var two = prompt("What gift did you get from " + one + "?");
-  var thr = prompt("What kind of party did " + one + " go to? (Do not include connecters, just the noun)");
+  var thr = prompt("What kind of party did " + one + " go to? (Do not include connectors, just the noun)");
   var fou = prompt("What is your name?");
   alert("Here is your letter so far. Press OK.");
   alert("Dear " + one + ", thank you so much for getting me a " + two + ". Thank you for coming to my " + thr + ". From, " + fou);
